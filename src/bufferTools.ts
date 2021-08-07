15d8bab8dfced035bba3349a69aa201fb9b1b449
Drazi Crendraven <drazisil@users.noreply.github.com>
2021-08-07 15:08:04 -0400

    create extendedBuffer class

--- src/bufferTools.ts
+++ src/bufferTools.ts
@@ -1,13 +1,50 @@
 /**
  * Remove count from inputBuffer and return both parts
- * @param inputBuffer 
- * @param count 
+ * @param inputBuffer
+ * @param count
  * @returns {Object} shiftedBuffer
  * @returns {Buffer} shiftedBuffer.value The value that has been removed
  * @returns {Buffer} shiftedBuffer.remainder The remainder of the buffer
  */
-export function shiftBuffer(inputBuffer: Buffer, count:number): { value: Buffer, remainder: Buffer} {
-    const value = Buffer.from(inputBuffer.slice(0, count))
-    const remainder = Buffer.from(inputBuffer.slice(count))
-    return { value, remainder}
+export function shiftBuffer(
+  inputBuffer: Buffer,
+  count: number
+): { value: Buffer; remainder: Buffer } {
+  const value = Buffer.from(inputBuffer.slice(0, count));
+  const remainder = Buffer.from(inputBuffer.slice(count));
+  return { value, remainder };
+}
+
+export class ExtendedBuffer {
+    private _buffer: Buffer
+
+    /**
+     * Wraps the native Buffer object to provide extra functionality
+     * @param inputBuffer {Buffer}
+     */
+    constructor(inputBuffer: Buffer) {
+        this._buffer = inputBuffer
+    }
+
+  /**
+   * Remove count from self removed part, return value
+   * value of the property buffer is set to remainder
+   * @param inputBuffer
+   * @param count
+   * @returns {Buffer} The value that has been removed
+   */
+  public shiftBuffer(count: number): ExtendedBuffer {
+    const result = shiftBuffer(Buffer.from(this._buffer), count)
+    this._buffer = result.remainder
+    return new ExtendedBuffer(result.value)
+  }
+
+  public get rawBuffer(): Buffer {
+      return this._buffer
+  }
+
+  public toString(): string {
+      return this.rawBuffer.toString()
+  }
+  
 }
--- test/bufferTools.test.ts
+++ test/bufferTools.test.ts
@@ -1,4 +1,4 @@
-import { shiftBuffer} from '../src/bufferTools'
+import { ExtendedBuffer, shiftBuffer} from '../src/bufferTools'
 
 const testBuffer = Buffer.from('I_am_a_test_buffer')
 
@@ -14,6 +14,28 @@ describe('shiftBuffer()', () => {
     it('return a value and remainder that combine to match inputBuffer', () => {
         const result = shiftBuffer(testBuffer, 7)
         expect(Buffer.concat([result.value, result.remainder])).toEqual(testBuffer)
+    })
+
+})
+
+describe('ExtendedBuffer class', () => {
+    let extendedTestBuffer = new ExtendedBuffer(Buffer.alloc(0))
+    beforeEach(() => {
+        extendedTestBuffer = new ExtendedBuffer(testBuffer)
+    })
+
+    it('returns a value', () => {
+        expect(extendedTestBuffer.shiftBuffer(7).toString()).toEqual('I_am_a_')
+    })
+
+    it('returns a remainder', () => {
+        const value = extendedTestBuffer.shiftBuffer(7).toString()
+        expect(extendedTestBuffer.toString()).toEqual('test_buffer')
+    })
+
+    it('return a value and remainder that combine to match inputBuffer', () => {
+        const value = extendedTestBuffer.shiftBuffer(7)
+        expect(Buffer.concat([value.rawBuffer, extendedTestBuffer.rawBuffer]).toString()).toEqual(testBuffer.toString())
     })
 
 })
